d7cf8842fe3182637499c36ddc8d007b1c184505
Brent Baccala <cosine@freesoft.org>
2024-12-03 17:40:41 -0500

    build_systems: use an allowed_bits bitstring that results in a dramatic performance improvement

--- build_systems.cc
+++ build_systems.cc
@@ -24,18 +24,21 @@
  *
  * ALGORITHM:
  *
- * Start with a bit string of all zeros (an empty ideal), and an empty list of finished bit strings,
- * but note that we might remove a bit string from the list of finished bit strings,
- * so they're not completed "finished".
+ * Start with a bit string of all zeros (an empty ideal), a bit string of all ones (the allowed factors),
+ * and an empty list of finished bit strings, but note that we might remove a bit string from the list of
+ * finished bit strings, so they're not completely "finished".  (Or maybe not, as we don't seem to be
+ * removing anything from the finished bit strings)
  *
  * We track the number of the polynomial we're working on.  First check to see if there's
  * any overlap between the polynomial's bit string and the working bit string (bitwise AND).
  * If so, this polynomial is satisfied; move on to the next one.
  *
- * Otherwise, we loop over the bits in polynomial's bit string.  For each one, we form
+ * Otherwise, we loop over the bits in polynomial's bit string that are also in the allowed
+ * factors (logical AND).  If there are none, then we discard this case and backtrack.  For each allowed one bit, we form
  * a new bit string by adding that one bit to the working bit string.  This will produce
- * a set of bit strings that we keep in a list.  Alternately, we track which bit we're
- * working on, since we need to backtrack.
+ * a set of bit strings that we keep in a list.  We remove the remaining bits
+ * from the allowed bits, since there is no reason to consider them later, as these
+ * factors will be considered when we backtrack to this point.
  *
  * See https://stackoverflow.com/questions/68798413/blsi-instruction-isolate-lowest-set-bit
  *
@@ -53,12 +56,9 @@
  * the working bit string, and move on to the next one.
  *
  * For each point in the stack of backtrack points:
- *    - working bit string
+ *    - working ideal bit string
+ *    - allowed bits bit string
  *    - polynomial number to process next
- *
- * If we don't include the bit number in the backtrack data, then we need to add a
- * set of backtrack points for each polynomial with multiple factors.  Doing it this
- * way probably makes it easier to implement multi-threading.
  *
  * Keep the stack of backtrack points as a queue.
  *
@@ -78,6 +78,7 @@
  *
  * POLYNOMIAL BIT STRINGS:
  *    - polys[i] are the complete bit strings for the i'th polynomial
+ *    - inverted_polys[i] are the bit-wise complements of polys[i]
  *    - expanded_polys[i] is a vector of bit strings, one vector for each polynomial, each bit string with a single bit set
  *
  * FINISHED BIT STRINGS:
@@ -139,8 +140,19 @@ public:
 
   friend std::ostream& operator<<(std::ostream& stream, BitString bs);
 
-  /* I don't want to copy return values, and operator& is only used in a boolean context. */
-#if 0
+  /* Bitwise NOT */
+
+  BitString operator~() const
+  {
+    BitString *rv = new BitString(len);
+    for (size_type i=0; i<bitstring.size(); i++) {
+      rv->bitstring[i] = ~ bitstring[i];
+    }
+    return *rv;
+  }
+
+  /* Bitwise AND returning a BitString */
+
   BitString operator&(const BitString& rhs) const
   {
     BitString *rv = new BitString(len);
@@ -149,8 +161,10 @@ public:
     }
     return *rv;
   }
-#else
-  bool operator&(const BitString& rhs) const
+
+  /* Bitwise AND returning a boolean */
+
+  bool operator&&(const BitString& rhs) const
   {
     for (size_type i=0; i<bitstring.size(); i++) {
       if (bitstring[i] & rhs.bitstring[i])
@@ -158,7 +172,6 @@ public:
     }
     return false;
   }
-#endif
 
   /* I don't want to copy return values, so use logical_or_assign instead. */
 #if 0
@@ -263,10 +276,12 @@ std::ostream& operator<<(std::ostream& stream, BitString bs)
 struct BacktrackPoint
 {
   BitString bitstring;
+  BitString allowed_bits;
   unsigned int next_polynomial;
 };
 
 std::vector<BitString> polys;
+std::vector<BitString> inverted_polys;
 std::vector<std::vector<BitString>> expanded_polys;
 
 LockingQueue<BacktrackPoint> backtrack_queue;
@@ -326,6 +341,7 @@ void task(void)
    */
   BacktrackPoint current_work;
   BitString next_work_bitstring;
+  BitString next_work_allowed_bits;
   BacktrackPoint extra_work;
 
   while (true) {
@@ -336,37 +352,55 @@ void task(void)
 
     /* I put this here because while the work was on the queue, we could have added new finished bitstrings */
     /* There's probably a race condition here, as a new finished bitstring could appear at any time! */
-    if (finished_bitstrings.contain_a_subset_of(current_work.bitstring)) continue;
+    if (finished_bitstrings.contain_a_subset_of(current_work.bitstring)) {
+      std::cerr << "detected superset\n";
+      continue;
+    }
 
     while (current_work.next_polynomial < polys.size()) {
       /* If there's any overlap here, the polynomial is already satisfied, move on */
       // std::cerr << "working on " << current_work.bitstring << "\n";
-      if (! (current_work.bitstring & polys[current_work.next_polynomial])) {
+      if (! (current_work.bitstring && polys[current_work.next_polynomial])) {
 	bool have_next_work = false;
 	/* Extend backtrack queue if there's more than one factor(bit) in the polynomial */
 	for (BitString next_bit: expanded_polys[current_work.next_polynomial]) {
-	  // std::cerr << "adding " << next_bit << "\n";
-	  if (! have_next_work) {
-	    /* Like this, but avoids a copy */
-	    /* next_work_bitstring = current_work.bitstring | next_bit; */
-	    current_work.bitstring.logical_or_assign(next_work_bitstring, next_bit);
-	    /* Check first if this is a superset of an existing bit string; skip it if it is */
-	    if (finished_bitstrings.contain_a_subset_of(next_work_bitstring)) continue;
-	    have_next_work = true;
-	  } else {
-	    /* Like this, but avoids a copy */
-	    /* extra_work.bitstring = current_work.bitstring | next_bit; */
-	    current_work.bitstring.logical_or_assign(extra_work.bitstring, next_bit);
-	    extra_work.next_polynomial = current_work.next_polynomial + 1;
-	    /* Check first if this is a superset of an existing bit string; skip it if it is */
-	    if (finished_bitstrings.contain_a_subset_of(extra_work.bitstring)) continue;
-	    backtrack_queue.push(extra_work);
+	  // std::cerr << "adding " << next_bit << " " << current_work.allowed_bits << "\n";
+	  /* skip this bit if it's not in the allowed_bits set */
+	  if (next_bit && current_work.allowed_bits) {
+	    if (! have_next_work) {
+	      /* Like this, but avoids a copy */
+	      /* next_work_bitstring = current_work.bitstring | next_bit; */
+	      current_work.bitstring.logical_or_assign(next_work_bitstring, next_bit);
+	      /* remove all of the current polynomial's bits from the allowed bits for future work */
+	      next_work_allowed_bits = current_work.allowed_bits & inverted_polys[current_work.next_polynomial];
+	      // std::cerr << "inverted_poly " << inverted_polys[current_work.next_polynomial] << "\n";
+	      // std::cerr << "next_work_allowed_bits " << next_work_allowed_bits << "\n";
+	      /* Check first if this is a superset of an existing bit string; skip it if it is */
+	      if (finished_bitstrings.contain_a_subset_of(next_work_bitstring)) {
+		std::cerr << "detected superset\n";
+		continue;
+	      }
+	      have_next_work = true;
+	    } else {
+	      /* Like this, but avoids a copy */
+	      /* extra_work.bitstring = current_work.bitstring | next_bit; */
+	      current_work.bitstring.logical_or_assign(extra_work.bitstring, next_bit);
+	      extra_work.allowed_bits = current_work.allowed_bits & inverted_polys[current_work.next_polynomial];
+	      extra_work.next_polynomial = current_work.next_polynomial + 1;
+	      /* Check first if this is a superset of an existing bit string; skip it if it is */
+	      if (finished_bitstrings.contain_a_subset_of(extra_work.bitstring)) {
+		std::cerr << "detected superset\n";
+		continue;
+	      }
+	      backtrack_queue.push(extra_work);
+	    }
 	  }
 	}
 	if (! have_next_work) {
 	  goto get_next_work_from_queue;
 	}
 	current_work.bitstring = next_work_bitstring;
+	current_work.allowed_bits = next_work_allowed_bits;
       }
       current_work.next_polynomial ++;
     }
@@ -378,6 +412,7 @@ void task(void)
 int main(int argc, char ** argv)
 {
   int nthreads = 1;
+  int bitstring_len = 0;
 
   if (argc > 1) {
     nthreads = std::stoi(argv[1]);
@@ -385,9 +420,21 @@ int main(int argc, char ** argv)
 
   std::string bitstring;
   while (std::getline(std::cin, bitstring)) {
+    /* BitString's constructor can take a string */
     polys.emplace_back(bitstring);
-    expanded_polys.emplace_back();
+
     BitString bs(bitstring);
+    if (bitstring_len == 0) {
+      bitstring_len = bs.len;
+    } else if (bitstring_len != bs.len) {
+      std::cerr << "bitstring lengths aren't consistent\n";
+      exit(1);
+    }
+
+    inverted_polys.emplace_back(~bs);
+
+    /* Put an empty list at the back of expanded_polys and fill it with the individual bits */
+    expanded_polys.emplace_back();
     BitString rmsb;
     do {
       rmsb = bs.rightmost_set_bit();
@@ -397,6 +444,8 @@ int main(int argc, char ** argv)
   }
 
   BacktrackPoint initial_work;
+  initial_work.allowed_bits = ~ BitString(bitstring_len);
+  // std::cerr << "allowed_bits " << initial_work.allowed_bits << "\n";
   initial_work.next_polynomial = 0;
   backtrack_queue.push(initial_work);
   backtrack_queue.set_num_workers(nthreads);
